fdfb5182675ef61bb66a530cc094b3cecdf8987c
dekarboro <oleksandr.ga@gmail.com>
2024-08-12 17:21:01 +0200

    final structure, main page + placeholders for other pages

--- scripts.js
+++ scripts.js
@@ -2,14 +2,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const mobileNavLinks = document.querySelector('.mobile-nav-links');
 
-
-    const headerLogo = document.querySelector('.header-logo');
-    const adjustLogo = () => {
-        headerLogo.classList.toggle('.header-logo-opened')
-    }
-
     menuToggle.addEventListener('click', function() {
         mobileNavLinks.classList.toggle('active');
-        headerLogo.classList.toggle('header-logo-opened')
     });
 });
+
+
