8bf5d68f5adcd15810c001f062cfcd7833764821
dekarboro <oleksandr.ga@gmail.com>
2024-08-20 00:03:02 +0200

    s

--- pl/scripts.js
+++ pl/scripts.js
@@ -1,36 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const mobileNavLinks = document.querySelector('.mobile-nav-links');
-    //const headerLogo = document.querySelector('.header-logo');
-    //const navMenu = document.querySelector('.nav-menu');
-    const headerImg = document.querySelector('.header-img');
-
-    const getHeight = elem => {
-        const h = elem.offsetHeight;
-        const styles = window.getComputedStyle(elem);
-        const mTop = parseInt(styles.marginTop, 10);
-        const mBot = parseInt(styles.marginBottom, 10);
-        
-        return h + mTop + mBot;
-    }
-
-    //const baseMargin = getHeight(navMenu);
-    //console.log(baseMargin);
-    //headerLogo.style.marginTop = `-${baseMargin}px`;
 
     menuToggle.addEventListener('click', function() {
-        mobileNavLinks.classList.toggle('active');
-        console.log('hi?')
-
-
-        //const navHeight = getHeight(navMenu);
-        //const mobileNavHeight = getHeight(mobileNavLinks);
-
-        // if (mobileNavLinks.classList.contains('active'))
-        //headerLogo.style.marginTop = `${(navHeight + mobileNavHeight) * -1}px`;
-        
+        mobileNavLinks.classList.toggle('active');        
     });
-
     
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
