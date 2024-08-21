18610ba5e372a36b5d66d045a4893882c1ef375d
dekarboro <oleksandr.ga@gmail.com>
2024-08-21 21:56:23 +0200

    classic

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const answer = document.querySelector('answer');
 
     menuToggle.addEventListener('click', function() {
-        mobileNavLinks.classList.toggle('active');        
+        mobileNavLinks.classList.toggle('active');
     });
     
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -28,26 +28,26 @@ document.addEventListener('DOMContentLoaded', function() {
     //     e.preventDefault();
     //     document.querySelector('.form-success').classList.toggle('on');
     // })
-
-    form.addEventListener("submit", function(event){
-        event.preventDefault();
-        
-        let formData = new FormData(this);
-        
-        fetch('send-email.php', {
-            method: 'POST',
-            body: formData
-        }).then(response => response.text())
-          .then(data => {
-              // Display the message in a div or alert
-              answer.innerText = data;
-              answer.classList.toggle('form-success');
-          }).catch(error => {
-                answer.innerText = error;
-                answer.classList.toggle('form-error');
-          });
-    });
-    
+    if (form) {
+        form.addEventListener("submit", function(event){
+            event.preventDefault();
+            
+            let formData = new FormData(this);
+            
+            fetch('send-email.php', {
+                method: 'POST',
+                body: formData
+            }).then(response => response.text())
+              .then(data => {
+                  // Display the message in a div or alert
+                  answer.innerText = data;
+                  answer.classList.toggle('form-success');
+              }).catch(error => {
+                    answer.innerText = error;
+                    answer.classList.toggle('form-error');
+              });
+        });
+    }
 });
 
 
