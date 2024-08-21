9ed9bd21c599f6295fc89a087e9d468eef9c541b
dekarboro <oleksandr.ga@gmail.com>
2024-08-21 22:15:58 +0200

    add captca

--- send-email.php
+++ send-email.php
@@ -8,23 +8,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['message']);
     $checkbox = htmlspecialchars($_POST['checkbox']);
 
-    // Set up email variables
-    $to = "dekarboro@gmail.com"; // Replace with your email address
-    $subject = "New Contact Form Submission by $firstName $lastName $email";
-    $body = "Name: $firstName $lastName\n
-            Email: $email\n
-            Phone: $phone\n
-            Checkbox: $checkbox\n\n
-            Message: \n$message";
-    $headers = "From: $email\r\n";
 
-    // Send the email
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Thank you! Your message have been sent successfully. We will contact you shortly.";
+    $recaptchaResponse = $_POST['g-recaptcha-response'];
+
+    $secretKey = "6LdCQywqAAAAAFPbl9Yzsmu-iSee5eqSUs1twfTv";
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secretKey&response=$recaptchaResponse&remoteip=$ip");
+    $responseKeys = json_decode($response, true);
+
+    if(intval($responseKeys["success"]) !== 1) {
+        echo "Please complete the CAPTCHA.";
     } else {
-        echo "Sorry, there was an error sending your message. Please try again later.";
-    }
-} else {
-    echo "Invalid request.";
-}
+        // CAPTCHA was completed successfully; proceed with form processing
+        $to = "project@galabazalt.com"; // Replace with your email address
+        $tome = "dekarboro@gmail.com"; // Replace with your email address
+        $subject = "New Contact Form Submission by $firstName $lastName $email";
+        $body = "Name: $firstName $lastName\n
+                Email: $email\n
+                Phone: $phone\n
+                Checkbox: $checkbox\n\n
+                Message: \n$message";
+        $headers = "From: $email\r\n";
+
+        mail($tome, $subject, $body, $headers);
+
+        // Send the email
+        if (mail($to, $subject, $body, $headers)) {
+            echo "Thank you! Your message have been sent successfully. We will contact you shortly.";
+        } else {
+            echo "Sorry, there was an error sending your message. Please try again later.";
+        }        
+    }    
 ?>
